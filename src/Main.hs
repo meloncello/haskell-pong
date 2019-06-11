e479c032313f353b8cda8d19fe721ef59615e136
Matteo Battisti <matteobattisti@protonmail.com>
2019-06-11 20:07:38 +0200

    fixed the collisions and refactored the code

--- src/Main.hs
+++ src/Main.hs
@@ -5,22 +5,24 @@ where
 import Graphics.Gloss
 
 data PongState = PongState 
-                        { ballVel :: Point
-                        , ballLoc :: Point
-                        , player1H :: Float
-                        , player2H :: Float }
+                        { ballVel       :: Point
+                        , ballLoc       :: Point  -- type Point = (Float, Float)
+                        , player1Y      :: Float
+                        , player2Y      :: Float }
 
 initialState :: PongState
 initialState = PongState 
-                        { ballVel = (10, -30)
-                        , ballLoc = (15,-15)
-                        , player1H = 40
-                        , player2H = -80 }
+                        { ballVel       = (10, -30)
+                        , ballLoc       = (15,-15)
+                        , player1Y      = 40
+                        , player2Y      = 40 } 
 
-width, height, ballRadius :: Num a => a
-width = 300
-height = 300
-ballRadius = 20
+width, height, ballRadius, paddleHeight, paddleWidth :: Num number => number
+width         = 300
+height        = 300
+ballRadius    = 20
+paddleHeight  = 100
+paddleWidth   = 20
 
 windowed :: String -> Display
 windowed title = InWindow title (width, height) (100, 100)
@@ -31,36 +33,36 @@ background = black
 render :: PongState -> Picture
 render state = pictures [ ball
                         , walls
-                        , mkPaddle rose (-width / 2)  $ player1H state
-                        , mkPaddle blue (width  / 2)  $ player2H state ] 
-          where
-            ball = uncurry translate (ballLoc state) $ color white $ circleSolid ballRadius
-            mkPaddle cl x y = let paddle = rectangleSolid 50 200
-                              in translate x y $ pictures [ color yellow $ rectangleSolid 60 210
-                                                          , color cl paddle]
-            walls = let wallColor = greyN 0.5
-                        wall offset = translate 0 offset $ color wallColor $ rectangleSolid 270 10
-                    in  pictures [wall (height/2), wall (-height/2)]
+                        , mkPaddle rose (-width / 2)  $ player1Y state
+                        , mkPaddle blue (width  / 2)  $ player2Y state ] 
+  where
+    ball = uncurry translate (ballLoc state) $ color white $ circleSolid ballRadius
+    mkPaddle cl x y = let paddle = rectangleSolid paddleWidth paddleHeight
+                      in translate x y $ pictures [ color yellow $ rectangleSolid (paddleWidth + 10) $ paddleHeight + 10
+                                                  , color cl paddle ]
+    walls = let wallColor = greyN 0.5
+                wall offset = translate 0 offset $ color wallColor $ rectangleSolid width 10
+            in  pictures [wall (height/2), wall (-height/2)]
 
 moveBall :: Float -> PongState -> PongState
 moveBall seconds state = state { ballLoc = (x',y') }
-                    where
-                      (x, y)   = ballLoc state 
-                      (vx, vy) = ballVel state
-                      [x', y'] = (\(a,b) [x, y] -> [x+a, y+b]) (x,y) $ (*seconds) <$> [vx, vy] 
+  where
+    (x, y)   = ballLoc state 
+    (vx, vy) = ballVel state
+    [x', y'] = (\(a,b) [x, y] -> [x+a, y+b]) (x,y) $ (*seconds) <$> [vx, vy] 
 
 wallCollision :: Point -> Float -> Bool
 wallCollision (_,y) radius = bottomCollision || topCollision
   where
-    bottomCollision = y - radius <= width / 2
-    topCollision    = y + radius >= width / 2
+    bottomCollision = y - radius <= -width / 2
+    topCollision    = y + radius >=  width / 2
 
 wallBounce :: PongState -> PongState
 wallBounce state = if wallCollision (ballLoc state) ballRadius
-                      then state { ballLoc = (x, -y) }
+                      then state { ballVel = (x, -y) }
                       else state
-              where
-                (x,y) = ballLoc state
+  where
+    (x,y) = ballLoc state
     
 
 
@@ -69,5 +71,5 @@ main = simulate (windowed "pong - test") background 60 initialState render updat
   where
     frame :: Float -> Picture
     frame seconds = render $ moveBall seconds initialState
-    update _ time = moveBall time . wallBounce
+    update _ time = wallBounce . moveBall time
   
