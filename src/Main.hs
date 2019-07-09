1c6c06c5816de28dad60040feb0884e7d05a8192
Matteo Battisti <matteobattisti@protonmail.com>
2019-07-09 17:39:15 +0200

    created Lib.hs

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@ module Main
 where
 
 import Graphics.Gloss
+import Graphics.Gloss.Interface.Pure.Game
 
 data PongState = PongState 
   { ballVel       :: Point
@@ -28,9 +29,9 @@ data GameState = GameState
 
 initialPongState :: PongState
 initialPongState = PongState 
-  { ballVel       = (10, -30)
+  { ballVel       = (15, -15)
   , ballLoc       = (15,-15)
-  , player1Y      = 0
+  , player1Y      = 50
   , player2Y      = 0 }
 
 gameWin :: GameWindow
@@ -51,9 +52,6 @@ gameState   = GameState
   , window  = gameWin
   , ball    = gameBall  }
 
-width'  = width gameWin  
-height' = height gameWin 
-(pw,ph) = paddle gameWin 
 ---------------------------------------------------------------------------
 windowed :: String -> GameWindow -> Display
 windowed title gwin = InWindow title (width', height') (100,100)
@@ -62,6 +60,7 @@ windowed title gwin = InWindow title (width', height') (100,100)
     width'  = (floor $ width gwin)
     height' :: Int
     height' = (floor $ height gwin)
+
 render :: GameState -> Picture
 render gstate = pictures  [ ball''
                           , walls
@@ -69,13 +68,14 @@ render gstate = pictures  [ ball''
                           , paddle'
                           , paddle'' ] 
   where
-    pstate  = state gstate
-    win     = window gstate
-    ball'   = ball gstate
-    width'  = width win
-    height' = height win   
-
-    ball''  = uncurry translate (ballLoc pstate) $
+    pstate    = state gstate
+    win       = window gstate
+    ball'     = ball gstate
+    width'    = width win
+    height'   = height win   
+    (pw, ph)  = paddle win     
+    
+    ball''    = uncurry translate (ballLoc pstate) $
                 Color (ballColor ball') $ circleSolid (radius ball')
 
     mkPaddle cl  = pictures [ Color yellow $ uncurry rectangleSolid $ (\(a,b) n -> (a+n, b+n)) (pw,ph) 10
@@ -97,38 +97,67 @@ moveBall seconds gstate = gstate { state = pstate { ballLoc = (x',y') }}
     (vx, vy)  = ballVel pstate
     [x', y']  = (\(a,b) [x, y] -> [x+a, y+b]) (x,y) $ (*seconds) <$> [vx, vy] 
 
-wallCollision :: Point -> Radius -> Bool
-wallCollision (_,y) radius = bottomCollision || topCollision
+wallCollision :: Point -> Radius -> GameWindow -> Bool
+wallCollision (_,y) radius gwin = bottomCollision || topCollision
   where
+    width' = width gwin
     bottomCollision = y - radius <= -width' / 2
     topCollision    = y + radius >=  width' / 2
 
 wallBounce :: GameState -> GameState
-wallBounce gstate = if wallCollision (x,y) (radius $ ball gstate)
+wallBounce gstate = if wallCollision (x,y) (radius $ ball gstate) (window gstate)
                       then gstate { state = pstate { ballVel = (x, -y) }}
                       else gstate
   where
     pstate  = state gstate
     (x,y)   = ballLoc pstate
 
-paddleCollision :: Point -> Radius -> (Float, Float) -> Bool
-paddleCollision (x,y) radius (middlePL, middlePR) = leftCollision || rightCollision
+paddleCollision :: Point -> Point -> Radius -> GameWindow -> Bool
+paddleCollision (x,y) (mp1, mp2) radius gwin = leftCollision || rightCollision
   where
-    leftCollision             = x + radius >=  width'/2 - pw / 2 && hasSameY middlePL
-    rightCollision            = x - radius >=  width'/2 + pw / 2 && hasSameY middlePR
-    hasSameY mp = (y + radius < ph || y + radius > mp - ph / 2 || y - radius <= ph)
+    width'          = width gwin
+    (pw,ph)         = paddle gwin 
+    leftCollision   = x - radius <=  (-width' + pw) / 2 && hasSameY mp1
+    rightCollision  = x + radius >=  ( width' - pw) / 2 && hasSameY mp2
+    hasSameY mp     = (y + radius > mp - ph / 2 && y - radius <= ph)
+
 paddleBounce :: GameState -> GameState 
-paddleBounce gstate = if paddleCollision (x,y) (radius $ ball gstate) (player1Y pstate, player2Y pstate)
+paddleBounce gstate = if paddleCollision (x,y) (p1Y,p2Y) (radius $ ball gstate) (window $ gstate)
                         then gstate { state = pstate { ballVel = (-x, y) }}
                         else gstate
     where
       pstate  = state gstate
-      (x,y)   =  ballLoc pstate
+      (x,y)   = ballLoc pstate
+      p1Y     = player1Y $ pstate
+      p2Y     = player2Y $ pstate
 
 main :: IO ()
-main = simulate (windowed "pong - test" gameWin) (background gameWin) 60 gameState render update
+main = play (windowed "pong - test" gameWin) (background gameWin) 60 gameState render handleKeys update
   where
     frame :: Float -> Picture
     frame seconds = render $ moveBall seconds gameState
-    update _ time = paddleBounce . wallBounce . moveBall time
-     
+    
+    update time = paddleBounce . wallBounce . moveBall time 
+    
+    handleKeys :: Event -> GameState -> GameState
+    handleKeys (EventKey key Down _ _) game 
+      | key == SpecialKey KeyUp || key == SpecialKey KeyDown  = player1Handler key
+      | key == Char 'w' || key == Char 's'                    = player2Handler key
+      | otherwise                                             = game
+      where
+        pstate  = state game
+        win         = window game
+        pOneY       = player1Y pstate
+        pTwoY       = player2Y pstate
+        halfPaddle  = (snd $ paddle win) / 2
+        halfHeight  = (height win) / 2
+
+        player1Handler key 
+          | key == SpecialKey KeyUp && pOneY + halfPaddle + 10 <= halfHeight    = game { state = pstate { player1Y = pOneY + 10 } }
+          | key == SpecialKey KeyDown && pOneY - halfPaddle - 10 >= -halfHeight = game { state = pstate { player1Y = pOneY - 10 } }
+          | otherwise                                                           = game 
+        player2Handler key
+          | key == Char 'w' && pTwoY + halfPaddle + 10 <= halfHeight  = game { state = pstate { player2Y = pTwoY + 10 } }
+          | key == Char 's' && pTwoY - halfPaddle - 10 >= -halfHeight = game { state = pstate { player2Y = pTwoY - 10 } }
+          | otherwise                                                 = game
+    handleKeys _ game = game                                         
